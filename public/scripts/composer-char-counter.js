4325daf6987f6be87538c979d015aa974b629620
TuanPham303 <phamcongtuan303@gmail.com>
2017-09-19 11:55:31 -0700

    update character counter and change color to red when exceeded 140 chars

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,10 @@
+/*jshint esversion: 6*/
+
+$("textarea").keyup(function(){
+  var len = this.value.length;
+  var remainChar = 140 - len;
+  $(".counter").html(remainChar);
+  if(remainChar < 0){
+    $(".counter").css("color", "red");
+  }
+}); 
